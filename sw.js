0c5f774b9150d11b8f34d28a16df4f172d5682ae
unknown <ankitsharma101299@gmail.com>
2020-04-11 00:46:58 +0530

    Added Changes to Service Worker

--- sw.js
+++ sw.js
@@ -15,40 +15,20 @@ self.addEventListener('install',e => {
     );
 })
 
-self.addEventListener('fetch', (event) => {
-    if (event.request.method === 'GET') {
-      event.respondWith(
-        caches.match(event.request)
-        .then((cached) => {
-          var networked = fetch(event.request)
-            .then((response) => {
-              let cacheCopy = response.clone()
-              caches.open(CACHE_NAME)
-                .then(cache => cache.put(event.request, cacheCopy))
-              return response;
+
+self.addEventListener('fetch',req => {
+    req.respondWith(
+        caches.match(req.request)
+        .then(resp => {
+            return resp || fetch(req.request).then(response => {
+                if(!response || response.status !== 200 || response.type !== 'basic') {
+                    return response;
+                  }
+                return caches.open(version).then(cache => {
+                    cache.put(req.request,response.clone());
+                    return response;
+                })
             })
-            .catch(() => caches.match(offlinePage));
-          return cached || networked;
         })
-      )
-    }
-    return;
-  });
-
-
-// self.addEventListener('fetch',req => {
-//     req.respondWith(
-//         caches.match(req.request)
-//         .then(resp => {
-//             return resp || fetch(req.request).then(response => {
-//                 if(!response || response.status !== 200 || response.type !== 'basic') {
-//                     return response;
-//                   }
-//                 return caches.open(version).then(cache => {
-//                     cache.put(req.request,response.clone());
-//                     return response;
-//                 })
-//             })
-//         })
-//     )    
-// })
+    )    
+})

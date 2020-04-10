7a96667463774d4bd57e06a9007f0c4adcfed05e
unknown <ankitsharma101299@gmail.com>
2020-04-11 00:41:20 +0530

    Added Changes to Service Worker

--- sw.js
+++ sw.js
@@ -1,42 +1,34 @@
-var version = "portfolio";
+var version = 'portfolio';
 
 var cachedAssets = [
-  "/Portfolio/",
-  "/Portfolio/index.html",
-  "/Portfolio/assets/images/goup.png",
-  "/Portfolio/assets/images/waving-hand.png",
+    '/Portfolio/',
+    '/Portfolio/index.html',
+    '/Portfolio/assets/images/goup.png',
+    '/Portfolio/assets/images/waving-hand.png'
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches
-      .open(version)
-      .then((cache) => cache.addAll(cachedAssets))
-      .catch((err) => console.log("Error"))
-  );
-});
-
-self.addEventListener("fetch", (req) => {
-  if (req.request.method === "GET") {
-    req.respondWith(
-      caches.match(req.request).then((resp) => {
-        return (
-          resp ||
-          fetch(req.request).then((response) => {
-            if (
-              !response ||
-              response.status !== 200 ||
-              response.type !== "basic"
-            ) {
-              return response;
-            }
-            return caches.open(version).then((cache) => {
-              cache.put(req.request, response.clone());
-              return response;
-            });
-          })
-        );
-      })
+self.addEventListener('install',e => {
+    e.waitUntil(
+        caches.open(version)
+        .then(cache => cache.addAll(cachedAssets))
+        .catch(err => console.log('Error'))
     );
-  }
-});
+})
+
+
+self.addEventListener('fetch',req => {
+    req.respondWith(
+        caches.match(req.request)
+        .then(resp => {
+            return resp || fetch(req.request).then(response => {
+                if(!response || response.status !== 200 || response.type !== 'basic') {
+                    return response;
+                  }
+                return caches.open(version).then(cache => {
+                    cache.put(req.request,response.clone());
+                    return response;
+                })
+            })
+        })
+    )    
+})

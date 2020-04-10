33273a55a4378345c852ff87e9b82e961ab62217
unknown <ankitsharma101299@gmail.com>
2020-04-10 14:04:53 +0530

    Added service worker for site

---
+++ assets/scripts/main.js
@@ -0,0 +1,8 @@
+if ("ServiceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("../../sw_cached_pages.js")
+      .then((reg) => console.log(`Service Worker Registered : ` + reg))
+      .catch((err) => console.log(err));
+  });
+}

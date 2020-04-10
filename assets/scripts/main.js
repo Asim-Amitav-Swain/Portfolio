cf57abacdddc329bdf176ab74e36c9abaea28472
unknown <ankitsharma101299@gmail.com>
2020-04-10 16:03:52 +0530

    Updated site

--- assets/scripts/main.js
+++
@@ -1,8 +0,0 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("assets/scripts/sw_cached_pages.js")
-      .then((reg) => console.log(`Service Worker Registered : ` + reg))
-      .catch((err) => console.log(err));
-  });
-}
